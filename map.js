a91db02a65d9536513cfd7f8094ec7c194e5f7e4
sfdduncan <164909361+sfdduncan@users.noreply.github.com>
2024-05-04 13:35:47 -0400

    Update map.js

--- map.js
+++ map.js
@@ -62,6 +62,9 @@ function downloadImage(mapUrl) {
             document.body.removeChild(link);
             window.URL.revokeObjectURL(blobUrl); 
         })
-        .catch(error => console.error('Error downloading the image:', error));
+        .catch(error => { 
+            console.error('Error downloading the image:', error);
+            alert('Error processing your request.'); 
+        });
 }
     
